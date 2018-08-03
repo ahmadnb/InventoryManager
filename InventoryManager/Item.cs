f7b3e918b174e98a088938f081b6810dc47b78fc
ahmadnb <ahmadnb@yahoo.com>
2018-08-03 02:20:12 -0500

    Initial put.

---
+++ InventoryManager/Item.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager
+{
+    class Item
+    {
+        // Private members of the Item class
+        private string name;
+        private int sellin;
+        private int quality;
+
+        // Constructors
+        public Item() { }
+
+        public  Item(Item i)
+        {
+            this.name = i.name;
+            this.sellin = i.sellin;
+            this.quality = i.quality;
+        }
+
+        public Item(string n, int s, int q)
+        {
+            this.name = n;
+            this.sellin = s;
+            this.quality = q;
+        }
+
+        /***********************************************************************
+         * GetName
+         * 
+         * Public method used to retrieve the item's name
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * None
+         ***********************************************************************
+         * Output:
+         * 
+         * Item Name
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        /***********************************************************************
+         * GetSellin
+         * 
+         * Public method used to retrieve the item's sellin value
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * None
+         ***********************************************************************
+         * Output:
+         * 
+         * Item Sell-in value
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        public int GetSellin()
+        {
+            return this.sellin;
+        }
+
+        /***********************************************************************
+         * GetQuality
+         * 
+         * Public method used to retrieve the item's quality value
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * None
+         ***********************************************************************
+         * Output:
+         * 
+         * Item Quality value
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        public int GetQuality()
+        {
+            return this.quality;
+        }
+
+        /***********************************************************************
+         * ValidateName
+         * 
+         * Private method used to check to see if the name entered by the user is 
+         * a valid name
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * Item name
+         ***********************************************************************
+         * Output:
+         * 
+         * If the input item name is valid, it is left unchanged.
+         * Otherwise it will be changed to "NO SUCH ITEM".
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        public string ValidateName(string n)
+        {
+            n = n.Trim();
+
+            if (!((n.ToLower() == "aged brie")
+                || (n.ToLower() == "backstage passes")
+                || (n.ToLower() == "conjured")
+                || (n.ToLower() == "normal item")
+                || (n.ToLower() == "sulfuras")))
+            {
+                n = "NO SUCH ITEM";
+            }
+
+            return n;
+        }
+
+        /***********************************************************************
+         * ProcessItem
+         * 
+         * Public method used to update the item sellin and quality values after 
+         * 1 day
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * None
+         ***********************************************************************
+         * Output:
+         * 
+         * None
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        public void ProcessItem()
+        {
+            // The item "Sulfaras" has no expiry date and does not decrease in quality
+            if (this.name.ToLower() != "sulfuras")
+            {
+                this.sellin--;
+            }
+
+            // All other items will experience changes to quality one way or another
+            if (this.name.ToLower() == "aged brie")
+            {
+                UpdateQualityAgedBrie();
+            }
+            else if (this.name.ToLower() == "backstage passes")
+            {
+                UpdateQualityBackstagePasses();
+            }
+            else if (this.name.ToLower() == "conjured")
+            {
+                UpdateQualityConjured();
+            }
+            else if (this.name.ToLower() == "normal item")
+            {
+                UpdateQualityNormalItem();
+            }
+                        
+            if (this.quality > 50)
+            { // Item quality cannot be greater than 50
+                this.quality = 50;
+            }
+            else if (this.quality < 0)
+            { // Item quality cannot be less than 0
+                this.quality = 0;
+            }
+        }
+
+        /***********************************************************************
+         * UpdateQualityAgedBrie
+         * 
+         * Private method used to update the quality of a backstage pass item
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * None
+         ***********************************************************************
+         * Output:
+         * 
+         * None
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        private void UpdateQualityAgedBrie()
+        {
+            // Item quality improves with time
+            this.quality++;
+        }
+
+
+        /***********************************************************************
+         * UpdateQualityBackstagePasses
+         * 
+         * Private method used to update the quality of a backstage pass item
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * None
+         ***********************************************************************
+         * Output:
+         * 
+         * None
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        private void UpdateQualityBackstagePasses()
+        {
+            if (this.sellin > 10)
+            { // Item quality increases when sell-in value is greater than 10
+                this.quality++;
+            }
+            else if ((this.sellin <= 10) && (this.sellin > 5))
+            { // Item quality increases at double the normal rate when sell-in value is less than or equal to 10 but greater than 5
+                this.quality += 2;
+            }
+            else if ((this.sellin <= 5) && (this.sellin >= 0))
+            { // Item quality increases at triple the normal rate when sell-in value is less than or equal to 5 but not expired
+                this.quality += 3;
+            }
+            else if (this.sellin < 0)
+            { // Item quality goes to 0 when this item has expired
+                this.quality = 0;
+            }
+        }
+
+        /***********************************************************************
+         * UpdateQualityConjured
+         * 
+         * Private method used to update the quality of a conjured item
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * None
+         ***********************************************************************
+         * Output:
+         * 
+         * None
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        private void UpdateQualityConjured()
+        {
+            // Item quality decrements at twice the normal rate
+            this.quality -= 2;
+
+            if (this.sellin < 0)
+            { // Item quality decrements at twice its usual rate if item has expired
+                this.quality -= 2;
+            }
+        }
+
+        /***********************************************************************
+         * UpdateQualityNormalItem
+         * 
+         * Private method used to update the quality of a normal item
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * None
+         ***********************************************************************
+         * Output:
+         * 
+         * None
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        private void UpdateQualityNormalItem()
+        {
+            this.quality--;
+
+            if (this.sellin < 0)
+            { // Item quality decrements at twice the normal rate if item has expired
+                this.quality--;
+            }
+        }
+    }
+}
--- InventoryManager/Program.cs
+++ InventoryManager/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace InventoryManager
 {
@@ -10,6 +8,132 @@ namespace InventoryManager
     {
         static void Main(string[] args)
         {
+            // Retrieve input arguments; there must be 1
+            int numArgs = args.Count<string>();
+
+            if (numArgs != 1)
+            {
+                Console.WriteLine("Need 1 parameter to proceed:");
+                Console.WriteLine("The full path including filename of a file containing item names, sellin values and quality values");
+                Environment.Exit(-1);
+            }
+
+            // Extract the name of the input file into a variable
+            string inputFilename = args[0].Trim();
+
+            // Process the contents of the input file; pass in the format for the output file name where the results of the execution will be published
+            ProcessLines(inputFilename + ".out", System.IO.File.ReadAllLines(inputFilename));
+            Console.WriteLine(Environment.NewLine + "Press any key to continue...");
+            Console.ReadLine();
+        }
+
+        /***********************************************************************
+         * ProcessLines
+         * 
+         * Private method used to process each line from the input file and 
+         * output the resulting item and its updated values to an output file
+         ***********************************************************************
+         * Input Parameters:
+         * 
+         * 1. Output filename
+         * 2. Array of all lines from the input file
+         ***********************************************************************
+         * Output:
+         * 
+         * None
+         ***********************************************************************
+         * Change history:
+         * 
+         * Date                Username               Changes made
+         * 
+         * 08/02/2018          Nabil Ahmad            Initial Creation
+         **********************************************************************/
+        static void ProcessLines(string outputFilename, string[] lines)
+        {
+            try
+            {
+                string path = @outputFilename;
+
+                // Remove the output file if it already exists
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                // Create the output file to write to
+                string createText = "Date: " + DateTime.Now.ToString("M/d/yyyy h:mm:ss tt") + Environment.NewLine;
+                File.WriteAllText(path, createText);
+
+                // Get the number of lines from the input file
+                int numLines = lines.Count<string>();
+
+                // Each line in the input file is a specific item that will be processed here
+                foreach (string line in lines)
+                {
+                    // Split each input line into individual strings
+                    string[] tokens = line.Split(' ');
+                    int numArgs = tokens.Count<string>();
+
+                    // Create a new item object
+                    Item i = new Item();
+                    string n = "";
+                    int s = 0;
+                    int q = 0;
+
+                    // Construct the item name from the input arguments
+                    for (int x = 0; x < (tokens.Count<string>() - 2); x++)
+                    {
+                        if ((n == "") && (x == 1))
+                        {
+                            n = tokens[x];
+                        }
+                        else
+                        {
+                            n += " " + tokens[x];
+                        }
+                    }
+
+                    // Validate the name of the item retrieved
+                    n = i.ValidateName(n.Trim());
+
+                    if (n == "NO SUCH ITEM")
+                    {
+                        Console.WriteLine(n);
+                        File.AppendAllText(path, n + Environment.NewLine);
+                        continue;
+                    }
+
+                    // Validate the sellin value retrieved
+                    if (!Int32.TryParse(tokens[numArgs - 2], out s))
+                    {
+                        Console.WriteLine("Invalid value of Sellin entered. Must be an integer.");
+                        File.AppendAllText(path, "Invalid value of Sellin entered. Must be an integer." + Environment.NewLine);
+                        continue;
+                    }
+
+                    // Validate the quality value retrieved
+                    if (!Int32.TryParse(tokens[numArgs - 1], out q))
+                    {
+                        Console.WriteLine("Invalid value of Quality entered. Must be an integer.");
+                        File.AppendAllText(path, "Invalid value of Quality entered. Must be an integer." + Environment.NewLine);
+                        continue;
+                    }
+
+                    // Create a new item based on the input values retrieved
+                    i = new Item(n, s, q);
+
+                    // Run the program on the item; update item sellin value and quality value after 1 day has passed
+                    i.ProcessItem();
+
+                    // Output the updated item and its member values in the same order (name, sellin value, quality) after 1 day to the console and to the output file
+                    Console.WriteLine(i.GetName() + " " + i.GetSellin().ToString() + " " + i.GetQuality().ToString());
+                    File.AppendAllText(path, i.GetName() + " " + i.GetSellin().ToString() + " " + i.GetQuality().ToString() + Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception encountered when attempting to read from the input file: " + ex.Message + ex.ToString());
+            }
         }
     }
 }
